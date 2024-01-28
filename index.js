ae51e9e0064f4cb15c39e63fcfb5be70ab7f7cc9
PythonXHunter <aryan.computerscience@gmail.com>
2024-01-28 18:31:45 +0530

    i was able to run the rpg game on node

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 5000;
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log('Server listening at ' + port);
+});
